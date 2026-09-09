2531593c26e3a892bd675cf3f662af415df95ed0
Ewin1802 <elantapa@gmail.com>
2026-09-09 11:41:08 +0800

    appserviceprovider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,7 +21,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        View::composer('layouts.partials.sidebar', function ($view) {
+        View::composer('components.sidebar', function ($view) {
             $view->with('totalMembers', MemberBarcode::count());
         });
     }
