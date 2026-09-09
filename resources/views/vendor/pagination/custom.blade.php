b0c83c9680fe98b459587d6937165c89482b5531
Ewin1802 <elantapa@gmail.com>
2026-09-09 12:58:06 +0800

    paginator

--- resources/views/vendor/pagination/custom.blade.php
+++ resources/views/vendor/pagination/custom.blade.php
@@ -7,18 +7,18 @@
 --}}
 
 @if ($paginator->hasPages())
-    <nav class="pagination-nav" aria-label="Navigasi halaman">
+    <nav class="trx-pagination" aria-label="Navigasi halaman">
 
-        <ul class="pagination-list">
+        <ul class="trx-pagination-list">
 
             {{-- PREVIOUS --}}
             @if ($paginator->onFirstPage())
-                <li class="page-item disabled" aria-disabled="true">
-                    <span class="page-link"><i data-lucide="chevron-left"></i></span>
+                <li class="trx-page disabled" aria-disabled="true">
+                    <span class="trx-page-link"><i data-lucide="chevron-left"></i></span>
                 </li>
             @else
-                <li class="page-item">
-                    <a href="{{ $paginator->previousPageUrl() }}" class="page-link" rel="prev">
+                <li class="trx-page">
+                    <a href="{{ $paginator->previousPageUrl() }}" class="trx-page-link" rel="prev">
                         <i data-lucide="chevron-left"></i>
                     </a>
                 </li>
@@ -28,18 +28,18 @@
             @foreach ($elements as $element)
 
                 @if (is_string($element))
-                    <li class="page-item disabled"><span class="page-link page-dots">{{ $element }}</span></li>
+                    <li class="trx-page disabled"><span class="trx-page-link trx-page-dots">{{ $element }}</span></li>
                 @endif
 
                 @if (is_array($element))
                     @foreach ($element as $page => $url)
                         @if ($page == $paginator->currentPage())
-                            <li class="page-item active" aria-current="page">
-                                <span class="page-link">{{ $page }}</span>
+                            <li class="trx-page active" aria-current="page">
+                                <span class="trx-page-link">{{ $page }}</span>
                             </li>
                         @else
-                            <li class="page-item">
-                                <a href="{{ $url }}" class="page-link">{{ $page }}</a>
+                            <li class="trx-page">
+                                <a href="{{ $url }}" class="trx-page-link">{{ $page }}</a>
                             </li>
                         @endif
                     @endforeach
@@ -49,14 +49,14 @@
 
             {{-- NEXT --}}
             @if ($paginator->hasMorePages())
-                <li class="page-item">
-                    <a href="{{ $paginator->nextPageUrl() }}" class="page-link" rel="next">
+                <li class="trx-page">
+                    <a href="{{ $paginator->nextPageUrl() }}" class="trx-page-link" rel="next">
                         <i data-lucide="chevron-right"></i>
                     </a>
                 </li>
             @else
-                <li class="page-item disabled" aria-disabled="true">
-                    <span class="page-link"><i data-lucide="chevron-right"></i></span>
+                <li class="trx-page disabled" aria-disabled="true">
+                    <span class="trx-page-link"><i data-lucide="chevron-right"></i></span>
                 </li>
             @endif
 
